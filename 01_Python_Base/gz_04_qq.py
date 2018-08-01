0a428e682baec69d2efba38ce8368ff1514ab811
gazerhuang <gazerhuang@gmail.com>
2018-08-01 22:12:42 +0800

    base

---
+++ 01_Python_Base/gz_04_qq.py
@@ -0,0 +1,7 @@
+# 1.定义一个变量记录QQ号码
+qq_number = "123456789"
+
+# 2.密码
+qq_password = "123"
+
+print(qq_number)
---
+++ 01_Python_Base/gz_05_apple.py
@@ -0,0 +1,10 @@
+# 1.定义苹果价格
+price = 8.5
+# 2.挑选苹果
+weight = 7.5
+# 3.计算付款金额
+money = weight * price
+# 4.买苹果立减5元
+money = money -5
+print(money)
+
